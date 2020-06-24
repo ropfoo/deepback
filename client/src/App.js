260bda7fc9b07050ee06a2516c3950be47f67305
RopFoo <ropfoo@gmail.com>
2020-06-24 22:33:54 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Letter from './components/Letter';
+//import './App.css';
+import './assets/sass/main.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <Letter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Letter.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Letter extends Component {
+  render() {
+    return <h2>My Letter</h2>;
+  }
+}
+
+export default Letter;
